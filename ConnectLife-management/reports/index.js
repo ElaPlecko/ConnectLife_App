77476d25b2cc107f90140f6e47b7bb0212e2c40d
masa_majcan <masa.majcan@student.um.si>
2026-05-30 19:26:06 +0200

    popravljeno geslo

--- ConnectLife-management/reports/index.js
+++ ConnectLife-management/reports/index.js
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "connectlife95@gmail.com",
-    pass: "zuvm objy xirz paf",
+    pass: "zuvmobjyxirzpaff",
   },
 });
 
