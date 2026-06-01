5781f313ea698336a71a7c8873837517160dfbd3
ElaPlecko <ela.plecko@student.um.si>
2026-06-01 15:40:52 +0200

    ven linki

--- ConnectLife-management/src/components/Sidebar.jsx
+++ ConnectLife-management/src/components/Sidebar.jsx
@@ -46,7 +46,6 @@ export default function Sidebar({ currentView, onNavigate, theme, setTheme, curr
        {navItem("markets", "Markets", "M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z")}
     {navItem("features", "Features", "M4 7h10M18 7h2M4 17h2M10 17h10M14 5v4M8 15v4")}
     {navItem("content", "Events", "M14 3H6a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9zM14 3v6h6M8 13h8M8 17h8M8 9h2")}
-    {navItem("links", "External Links", "M10 13a5 5 0 0 0 7.1 0l2.1-2.1a5 5 0 0 0-7.1-7.1L11 4.9M14 11a5 5 0 0 0-7.1 0l-2.1 2.1a5 5 0 0 0 7.1 7.1L13 19.1")}
 
     <p>Tools</p>
     {navItem("comparison", "Market Comparison", "M12 3v18M7 6h10M6 6l-3 7h6zM18 6l-3 7h6zM5 21h14")}
